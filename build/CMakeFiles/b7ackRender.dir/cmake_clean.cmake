5c04d3b5a5d9e375fff34bc17ef931502ce275d8
B7ackCa7 <b7ackca7@gmail.com>
2024-05-20 12:57:18 -0400

    initial commit

---
+++ build/CMakeFiles/b7ackRender.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/b7ackRender.dir/glad.c.o"
+  "CMakeFiles/b7ackRender.dir/glad.c.o.d"
+  "CMakeFiles/b7ackRender.dir/main.cpp.o"
+  "CMakeFiles/b7ackRender.dir/main.cpp.o.d"
+  "b7ackRender"
+  "b7ackRender.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/b7ackRender.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
